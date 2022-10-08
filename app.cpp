809831020187808cb40fae2a3f85178e3572149c
PENGUINLIONG <admin@penguinliong.moe>
2022-10-08 18:13:50 +0800

    Fractal (C-version)

--- app.cpp
+++ app.cpp
@@ -18,10 +18,10 @@ void clear_screen() {
   system("clear");
 #endif
 }
-void draw(const ti::NdArray<float>& canvas) {
+void draw(TiRuntime runtime, const TiNdArray& canvas) {
   clear_screen();
 
-  auto canvas_data = (const float*)canvas.map();
+  auto canvas_data = (const float*)ti_map_memory(runtime, canvas.memory);
   for (uint32_t y = 0; y < HEIGHT; ++y) {
     for (uint32_t x = 0; x < WIDTH; ++x) {
       float value = canvas_data[y * WIDTH + x];
@@ -29,29 +29,47 @@ void draw(const ti::NdArray<float>& canvas) {
     }
     std::cout << std::endl;
   }
-  canvas.unmap();
+  ti_unmap_memory(runtime, canvas.memory);
 }
 
 int main(int argc, const char** argv) {
-  ti::Runtime runtime(TI_ARCH_VULKAN);
+  TiRuntime runtime = ti_create_runtime(TI_ARCH_VULKAN);
 
-  ti::AotModule aot_module = runtime.load_aot_module("module");
-  ti::ComputeGraph fractal = aot_module.get_compute_graph("fractal");
+  TiAotModule aot_module = ti_load_aot_module(runtime, "module");
+  TiComputeGraph fractal = ti_get_aot_module_compute_graph(aot_module, "fractal");
 
-  ti::NdArray<float> canvas =
-    runtime.allocate_ndarray<float>({ WIDTH, HEIGHT }, {}, true);
+  TiMemoryAllocateInfo mai {};
+  mai.host_read = true;
+  mai.size = WIDTH * HEIGHT * sizeof(float);
+  TiMemory canvas_memory = ti_allocate_memory(runtime, &mai);
 
-  fractal["canvas"] = canvas;
+  TiNdArray canvas {};
+  canvas.elem_type = TI_DATA_TYPE_F32;
+  canvas.shape.dim_count = 2;
+  canvas.shape.dims[0] = WIDTH;
+  canvas.shape.dims[1] = HEIGHT;
+  canvas.memory = canvas_memory;
+
+  TiNamedArgument fractal_args[2];
+  fractal_args[0].name = "canvas";
+  fractal_args[0].argument.type = TI_ARGUMENT_TYPE_NDARRAY;
+  fractal_args[0].argument.value.ndarray = canvas;
 
   for (uint32_t i = 0; i < 10000; ++i) {
-    fractal["t"] = 0.03f * i;
+    fractal_args[1].name = "t";
+    fractal_args[1].argument.type = TI_ARGUMENT_TYPE_F32;
+    fractal_args[1].argument.value.f32 = 0.03f * i;
 
-    fractal.launch();
-    runtime.wait();
+    ti_launch_compute_graph(runtime, fractal, 2, fractal_args);
 
-    draw(canvas);
+    ti_wait(runtime);
+
+    draw(runtime, canvas);
     std::this_thread::sleep_for(0.03s);
   }
+
+  ti_destroy_aot_module(aot_module);
+  ti_destroy_runtime(runtime);
 
   return 0;
 }
